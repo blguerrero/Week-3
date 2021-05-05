7a98de20ea3841dc868a9944efc4f978145019d2
blguerrero <briannaguerrero890@gmail.com>
2021-05-05 16:27:55 -0700

    Notes from video 2 of week 3 - apply loop

---
+++ Video Notes - apply.R
@@ -0,0 +1,13 @@
+#Video notes from R Programming/Week 3/Loop Functions - apply
+str(mean)
+
+#Creating a matrix of 200 random nubers with 20 rows and 10 columns
+x <- matrix(rnorm(200), 20, 10)
+
+#to get the mean of each column
+#because in the apply function the second variable can only either
+#be a 1 or a 2 - 1 for row, 2 for columns
+apply(x, 2, mean)
+
+#to get the sum of each row
+apply(x, 1, sum)
