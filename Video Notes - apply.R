47811540ba9b8639a84bbf72a9b79267e064637d
blguerrero <briannaguerrero890@gmail.com>
2021-05-05 16:56:33 -0700

    added tapply notes

--- Video Notes - apply.R
+++ Video Notes - apply.R
@@ -15,4 +15,20 @@ apply(x, 1, sum)
 #how mapply makes life easier
 #before
 list(rep(1:4), rep(2,3), rep(3:2), rep(4:1))
+#after
 mapply(rep, 1:4, 4:1)
+
+#tapply
+#created a vector of 3 groups
+#Group 1 - 10 numbers from a normal dist
+#Group 2 - 10 numbers from a uniform
+#Group 3 - 10 numbers from normal with a mean of 1
+x <- c(rnorm(10), runif(10), rnorm(10,1))
+
+#create a second variable using gl function 
+#which just creates another factor variable with levels
+#create a factor variable that repeats 1, 2, and 3, 10 times (3 levels)
+f <-gl(3, 10)
+
+#tapply will run through x, group them by f, and take the mean of each section
+tapply(x, f, mean)
