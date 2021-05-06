931faf962f9e1cfae1d675f1bcd59854091ff5ba
blguerrero <briannaguerrero890@gmail.com>
2021-05-06 13:46:58 -0700

    Split notes

--- Video Notes - apply.R
+++ Video Notes - apply.R
@@ -32,3 +32,25 @@ f <-gl(3, 10)
 
 #tapply will run through x, group them by f, and take the mean of each section
 tapply(x, f, mean)
+
+#Split
+#does the same thing as above
+x <- c(rnorm(10), runif(10), rnorm(10,1))
+f <-gl(3, 10)
+split(x, f)
+lapply(split(x,f), mean)
+
+#Example using air quality dataframe
+library(datasets)
+head(airquality)
+
+#How can we calculate the mean of each variable by month
+#First, we have to split the data into months
+#Second, we calculate the means of each column using applyn or colMeans
+s <- split(airquality, airquality$Month)
+#airquality is your x, airquality$Month is what your are splitting your x by
+lapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")]))
+#lapply returns a vector of length 3 (ozone, solar, wind) for each month
+
+#to remove na's
+sapply(s, function(x) colMeans(x[, c("Ozone", "Solar.R", "Wind")], na.rm = TRUE))
