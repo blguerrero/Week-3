ab2609c46cdaa2d5c0fbb15dfccfd155a72f9dde
blguerrero <briannaguerrero890@gmail.com>
2021-05-07 10:49:04 -0700

    tapply notes

--- swirl - vapply & tapply.R
+++ swirl - vapply & tapply.R
@@ -9,5 +9,18 @@ vapply(flags, class, character(1))
 #works because the 'character(1)' argument tells R that we expect the
 #class function to return a character vector of length 1
 
-
+#tapply - split your data up into groups based on the value of some variable, 
+#then apply a function to the members of each group
+#landmass can be values between 1 and 6, diff part of world flag is from
+#see how manyflags/countries fall into each group
+table(flags$landmass)
+#animate takes on val of 1 if flag has animate image
+table(flags$animate)
+#see the mean of countries that have an animate object by landmass
+tapply(flags$animate, flags$landmass, mean)
+#see the summary of population values for countries with red on their flag
+#summary shows you min, mad, median, and 1st, and 3rd quartiles
+tapply(flags$population, flags$red, summary)
+#use the same approach to look at a summary of population values for each of the six landmasses
+tapply(flags$population, flags$landmass, summary)
 
