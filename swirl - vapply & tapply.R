69d12f8fb2aeb42b56e1aeee9b167b2ca359720c
blguerrero <briannaguerrero890@gmail.com>
2021-05-07 10:39:25 -0700

    vapply notes

--- swirl - vapply & tapply.R
+++ swirl - vapply & tapply.R
@@ -1,2 +1,13 @@
 library(swirl)
 swirl()
+#will be using the Flags dataset from UCI Machine Learning Repository
+#unique returns a vector of the unique values contained in the object passed to it
+sapply(flags, unique)
+vapply(flags, unique, numeric(1))
+#doesn't work because unique numeric length can be more than 1. 
+vapply(flags, class, character(1))
+#works because the 'character(1)' argument tells R that we expect the
+#class function to return a character vector of length 1
+
+
+
